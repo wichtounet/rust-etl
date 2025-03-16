33edb69d63b27c84beedcad34ce1cb3a2ded391a
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-03-16 20:16:24 +0100

    Make it generic

--- src/etl/vector.rs
+++ src/etl/vector.rs
@@ -1,35 +1,35 @@
-pub struct Vector {
-    data: Vec<i64>
+pub struct Vector<T: Default + Clone> {
+    data: Vec<T>
 }
 
-impl Vector {
+impl<T: Default + Clone> Vector<T> {
     pub fn new(size: usize) -> Self {
-        Self { data: vec![0; size] }
+        Self { data: vec![T::default(); size] }
     }
 
     pub fn size(&self) -> usize {
         self.data.len()
     }
 
-    pub fn iter(&self) -> std::slice::Iter<'_, i64> {
+    pub fn iter(&self) -> std::slice::Iter<'_, T> {
         self.data.iter()
     }
 
-    pub fn iter_mut(&mut self) -> std::slice::IterMut<'_, i64> {
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<'_, T> {
         self.data.iter_mut()
     }
 }
 
-impl std::ops::Index<usize> for Vector {
-    type Output = i64;
+impl<T: Default + Clone> std::ops::Index<usize> for Vector<T> {
+    type Output = T;
 
-    fn index(&self, index: usize) -> &i64 {
+    fn index(&self, index: usize) -> &T {
         &self.data[index]
     }
 }
 
-impl std::ops::IndexMut<usize> for Vector {
-    fn index_mut(&mut self, index: usize) -> &mut i64 {
+impl<T: Default + Clone> std::ops::IndexMut<usize> for Vector<T> {
+    fn index_mut(&mut self, index: usize) -> &mut T {
         &mut self.data[index]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@ mod etl;
 
 use crate::etl::vector::Vector;
 
-fn main() {
-    let mut vec: Vector = Vector::new(8);
+fn basic<TG: From<i32> + Default + Clone + std::fmt::Display>(size: usize) {
+    let mut vec: Vector<TG> = Vector::<TG>::new(size);
 
-    println!("Hello, world from a Vector({})!", vec.size());
+    println!("Hello, world from a Vector<{}>({})!", std::any::type_name::<TG>(), vec.size());
 
     for (n, value) in vec.iter_mut().enumerate() {
-        *value = (n * n) as i64;
+        let mut v: i32 = n as i32;
+        v *= v;
+        *value = v.into();
     }
 
     for value in vec.iter() {
         println!("{}", value);
     }
+}
+
+fn main() {
+    basic::<i64>(8);
+    basic::<f64>(8);
 }
