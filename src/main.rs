cee51a47b446cb3036236be7b923a74c0b4d9d6e
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-03-16 19:30:12 +0100

    Basic vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+
+
+pub struct Vector {
+    data: Vec<i64>
+}
+
+impl Vector {
+    pub fn new(size: usize) -> Self {
+        Self { data: vec![0; size] }
+    }
+
+    pub fn size(&self) -> usize {
+        self.data.len()
+    }
+}
+
+impl std::ops::Index<usize> for Vector {
+    type Output = i64;
+
+    fn index(&self, index: usize) -> &i64 {
+        &self.data[index]
+    }
+
+}
+
+impl std::ops::IndexMut<usize> for Vector {
+
+    fn index_mut(&mut self, index: usize) -> &mut i64 {
+        &mut self.data[index]
+    }
+
+}
+
 fn main() {
+    let mut vec: Vector = Vector::new(8);
+
+    for n in 0..vec.size() {
+        vec[n] = n as i64;
+    }
+
+    for n in 0..vec.size() {
+        println!("{}", n);
+    }
+
     println!("Hello, world!");
 }
