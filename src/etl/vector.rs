43dc0045e7b941177d343cc165e4be6669916f87
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-03-16 20:56:00 +0100

    Basic iterator (not great)

--- src/etl/vector.rs
+++ src/etl/vector.rs
@@ -1,6 +1,10 @@
+// The declaration of Vector<T>
+
 pub struct Vector<T: Default + Clone> {
     data: Vec<T>
 }
+
+// The functions of Vector<T>
 
 impl<T: Default + Clone> Vector<T> {
     pub fn new(size: usize) -> Self {
@@ -11,14 +15,20 @@ impl<T: Default + Clone> Vector<T> {
         self.data.len()
     }
 
-    pub fn iter(&self) -> std::slice::Iter<'_, T> {
-        self.data.iter()
+    pub fn iter(&self) -> VectorIterator<T> {
+        VectorIterator::<T> {
+            vector: self,
+            index: 0
+        }
     }
 
+    // Writing my own mutable iterator requires unsafe code (which I should do later)
     pub fn iter_mut(&mut self) -> std::slice::IterMut<'_, T> {
-        self.data.iter_mut()
+        return self.data.iter_mut();
     }
 }
+
+// Operator overloading for Vector<T>
 
 impl<T: Default + Clone> std::ops::Index<usize> for Vector<T> {
     type Output = T;
@@ -31,5 +41,28 @@ impl<T: Default + Clone> std::ops::Index<usize> for Vector<T> {
 impl<T: Default + Clone> std::ops::IndexMut<usize> for Vector<T> {
     fn index_mut(&mut self, index: usize) -> &mut T {
         &mut self.data[index]
+    }
+}
+
+// The declaration of VectorIterator<T>
+
+pub struct VectorIterator<'a, T: Default + Clone> {
+    vector: &'a Vector<T>,
+    index: usize
+}
+
+// The implementation of VectorIterator<T>
+
+impl<'a, T: Default + Clone> Iterator for VectorIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.vector.size() {
+            let result = Some(&self.vector[self.index]);
+            self.index += 1;
+            result
+        } else {
+            None
+        }
     }
 }
