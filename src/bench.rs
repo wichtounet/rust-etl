05bc0c9137b27d8b1fc9b24e8f4ae828d9233673
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-03-23 08:20:07 +0100

    Improve benchmark

--- src/bench.rs
+++ src/bench.rs
@@ -5,7 +5,27 @@ use crate::etl::vector::Vector;
 
 use std::time::SystemTime;
 
-fn bench_closure<F: FnMut()>(mut closure: F, rep: usize) -> (f64, f64) {
+fn choose_repeat<F: FnMut()>(mut closure: F) -> i64 {
+    let now = SystemTime::now();
+
+    for _n in 0..2 {
+        closure();
+    }
+
+    let elapsed = match now.elapsed() {
+        Ok(elapsed) => elapsed.as_nanos() as f64 / 2.0,
+        Err(e) => {
+            panic!("Time Error: {e:?}");
+        }
+    };
+
+    let rep = (2000000000.0 / elapsed) as i64;
+    rep
+}
+
+fn bench_closure<F: FnMut()>(mut closure: F) -> (f64, f64) {
+    let rep = choose_repeat(|| closure());
+
     let now = SystemTime::now();
 
     for _n in 0..rep {
@@ -30,18 +50,18 @@ fn choose_time(times: (f64, f64)) -> String {
     }
 }
 
-fn bench_basic_a(n: usize, r: usize) {
+fn bench_basic_a(n: usize) {
     let a = Vector::<f64>::new_rand(n);
     let b = Vector::<f64>::new_rand(n);
     let mut c = Vector::<f64>::new_rand(n);
 
     let func = || c |= &a + &b;
 
-    let times = bench_closure(func, r);
+    let times = bench_closure(func);
     println!("c = a + b ({}) took {}", n, choose_time(times));
 }
 
-fn bench_basic_b(n: usize, r: usize) {
+fn bench_basic_b(n: usize) {
     let a = Vector::<f64>::new_rand(n);
     let b = Vector::<f64>::new_rand(n);
     let c = Vector::<f64>::new_rand(n);
@@ -50,45 +70,64 @@ fn bench_basic_b(n: usize, r: usize) {
 
     let func = || d |= &a + &b + &c + &a;
 
-    let times = bench_closure(func, r);
+    let times = bench_closure(func);
     println!("d = a + b + c + a ({}) took {}", n, choose_time(times));
 }
 
-fn bench_gemv(rows: usize, columns: usize, r: usize) {
+fn bench_gemv(rows: usize, columns: usize) {
     let a = Matrix2d::<f64>::new_rand(rows, columns);
     let b = Vector::<f64>::new_rand(columns);
     let mut c = Vector::<f64>::new_rand(rows);
 
     let func = || c |= &a * &b;
 
-    let times = bench_closure(func, r);
+    let times = bench_closure(func);
     println!("c = M * v ({}:{}) took {}", rows, columns, choose_time(times));
 }
 
-fn main() {
-    bench_basic_a(1024, 65536);
-    bench_basic_a(8 * 1024, 32768);
-    bench_basic_a(16 * 1024, 16384);
-    bench_basic_a(32 * 1024, 8192);
-    bench_basic_a(64 * 1024, 4096);
-    bench_basic_a(128 * 1024, 2048);
-    bench_basic_a(1024 * 1024, 1024);
+fn bench_gemm(rows: usize, columns: usize, inner: usize) {
+    let a = Matrix2d::<f64>::new_rand(rows, inner);
+    let b = Matrix2d::<f64>::new_rand(inner, columns);
+    let mut c = Matrix2d::<f64>::new_rand(rows, columns);
 
-    bench_basic_b(1024, 32768);
-    bench_basic_b(8 * 1024, 16384);
-    bench_basic_b(16 * 1024, 8192);
-    bench_basic_b(32 * 1024, 4096);
-    bench_basic_b(64 * 1024, 2048);
-    bench_basic_b(128 * 1024, 1024);
-    bench_basic_b(1024 * 1024, 512);
+    let func = || c |= &a * &b;
+
+    let times = bench_closure(func);
+    println!("c = M * v ({}:{}:{}) took {}", rows, inner, columns, choose_time(times));
+}
+
+fn main() {
+    bench_basic_a(1024);
+    bench_basic_a(8 * 1024);
+    bench_basic_a(16 * 1024);
+    bench_basic_a(32 * 1024);
+    bench_basic_a(64 * 1024);
+    bench_basic_a(128 * 1024);
+    bench_basic_a(1024 * 1024);
+
+    bench_basic_b(1024);
+    bench_basic_b(8 * 1024);
+    bench_basic_b(16 * 1024);
+    bench_basic_b(32 * 1024);
+    bench_basic_b(64 * 1024);
+    bench_basic_b(128 * 1024);
+    bench_basic_b(1024 * 1024);
 
     // TODO: Also bench the performanc when combining GEMM and additions or scaling
 
-    bench_gemv(16, 128, 65536);
-    bench_gemv(32, 128, 32768);
-    bench_gemv(64, 256, 16384);
-    bench_gemv(128, 1024, 8192);
-    bench_gemv(256, 1024, 4095);
-    bench_gemv(1024, 256, 2048);
-    bench_gemv(1024, 1024, 1024);
+    bench_gemv(16, 128);
+    bench_gemv(32, 128);
+    bench_gemv(64, 256);
+    bench_gemv(128, 1024);
+    bench_gemv(256, 1024);
+    bench_gemv(1024, 256);
+    bench_gemv(1024, 1024);
+
+    bench_gemm(16, 128, 128);
+    bench_gemm(32, 128, 128);
+    bench_gemm(64, 256, 128);
+    bench_gemm(128, 1024, 256);
+    bench_gemm(256, 1024, 256);
+    bench_gemm(1024, 256, 512);
+    bench_gemm(1024, 1024, 1024);
 }
