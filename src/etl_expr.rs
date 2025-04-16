30ebddbbbd1aca66f4885974deea5bb66eaacd36
Baptiste Wicht <baptiste.wicht@gmail.com>
2025-04-16 06:53:20 +0200

    Abs trait

--- src/etl_expr.rs
+++ src/etl_expr.rs
@@ -68,6 +68,34 @@ impl Float for f64 {
     }
 }
 
+pub trait Abs {
+    fn abs(self) -> Self;
+}
+
+impl Abs for i32 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+}
+
+impl Abs for i64 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+}
+
+impl Abs for f32 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+}
+
+impl Abs for f64 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+}
+
 pub fn padded_size(size: usize) -> usize {
     (size + 7) & !7
 }
